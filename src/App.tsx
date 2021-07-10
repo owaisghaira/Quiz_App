fe212ceb841f4bcb8bbd7d1adfb61d0f7c2ec3ba
owaisghaira <owais.ghaira99@gmail.com>
2021-07-10 23:10:49 +0500

    get data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchQuestions } from "./Services/API";
+import QuestionCard from "./Components/QuestionCard";
+import { Difficulty, QuestionState, AnsObject } from "./Services/Utility";
+import { totalmem } from "os";
 
-function App() {
+const App: React.FC = () => {
+  const [loding, setLoding] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState<AnsObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const TotalQuestions: number = 10;
+
+  // console.log(questions[0].answer);
+
+  const start = async () => {
+    setLoding(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuestions(TotalQuestions, Difficulty.EASY);
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswer([]);
+    setNumber(0);
+    setLoding(false);
+  };
+
+  const checkAnswer = (e:any) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      console.log(answer);
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore((prev) => prev + 1);
+
+      //save answer
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswer((p: any) => [...p, answerObject]);
+      // console.log(score);
+    }
+  };
+
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQ = number + 1;
+
+    if (nextQ === TotalQuestions) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQ);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz_App</h1>
+      {gameOver || userAnswer.length === TotalQuestions ? (
+        <button onClick={start} className="start">
+          Start Quiz{" "}
+        </button>
+      ) : null}
+
+      {loding && <p>Londing...</p>}
+
+      {!gameOver ? <p className="score">Score:{score}</p> : null}
+      {!loding && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQues={TotalQuestions}
+          question={questions[number].question}
+          answers={questions[number].answer}
+          callback={checkAnswer}
+          userAns={userAnswer ? userAnswer[number] : undefined}
+        />
+      )}
+      {!gameOver &&
+      !loding &&
+      userAnswer.length === number + 1 &&
+      number !== TotalQuestions - 1 ? (
+        <button onClick={nextQuestion} className="next">
+          {" "}
+          Next
+        </button>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { AnsObject } from "../Services/Utility";
+type props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAns: AnsObject | undefined;
+  questionNr: number;
+  totalQues: number;
+};
+
+const QuestionCard: React.FC<props> = ({
+  question,
+  answers,
+  callback,
+  userAns,
+  questionNr,
+  totalQues,
+}) => {
+  return (
+    <>
+      <p>
+        Question : {questionNr} / {totalQues}
+      </p>
+      <div>{question}</div>
+      <div>
+        {/* <form onSubmit={callback}>
+          {answers.map((option: string, ind: number) => {
+            return (
+              <div key={ind}>
+                <label>
+                  <input type="radio" value={option} name="option" />
+                  {option}
+                </label>
+              </div>
+            
+            );
+          })}
+          <input type="submit" />
+        </form> */}
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAns ? true : false} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default QuestionCard;
